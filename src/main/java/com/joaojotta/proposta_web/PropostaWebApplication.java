620429f62004030236951113aba8d21dd8b750d1
JoaoSantos6 <78502928+JoaoSantos6@users.noreply.github.com>
2024-09-29 12:58:23 -0300

    Project created

---
+++ src/main/java/com/joaojotta/proposta_web/PropostaWebApplication.java
@@ -0,0 +1,13 @@
+package com.joaojotta.proposta_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropostaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropostaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaojotta/proposta_web/PropostaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaojotta.proposta_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
