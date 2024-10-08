108df30ad751579db772a0ef996df27aa3ca6c8a
JoaoSantos6 <78502928+JoaoSantos6@users.noreply.github.com>
2024-10-07 21:13:14 -0300

    Mapping Struct

--- src/main/java/com/joaojotta/proposta_web/entities/Proposta.java
+++ src/main/java/com/joaojotta/proposta_web/entities/Proposta.java
@@ -6,7 +6,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 @Entity
 public class Proposta {
 
--- src/main/java/com/joaojotta/proposta_web/entities/Usuario.java
+++ src/main/java/com/joaojotta/proposta_web/entities/Usuario.java
@@ -4,9 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 @Entity
 public class Usuario {
 
